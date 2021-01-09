e0561bb577e39e5ddabbf33a6e49c340018f04f8
sn2436 <snehalnibe11@gmail.com>
2021-01-09 16:09:08 +0530

    snehal file done

--- TeamProject/src/Snehal.java
+++ TeamProject/src/Snehal.java
@@ -3,6 +3,8 @@ public class Snehal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
               System.out.println("Snehal done!!!");
+              System.out.println("Done with git commit,push,pull,clone");
+           
 	}
 
 }
