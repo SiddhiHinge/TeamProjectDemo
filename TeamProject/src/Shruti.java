b57f572e2b95681be92a13b5903060297ee0d377
shruti1405 <jadhavshruthi14@gmail.com>
2021-01-09 16:03:50 +0530

    Second commit done by Shruti

--- TeamProject/src/Shruti.java
+++ TeamProject/src/Shruti.java
@@ -4,6 +4,8 @@ public class Shruti {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello......");
+		
+		System.out.println("Done with git commit,push,pull,clone");
 	}
 
 }
